cefa3e8ec0152183283adb5b73a5a858db874e82
Leo Chen <cswleo@gmail.com>
2024-11-22 12:54:54 +0800

    bfs

---
+++ BFS/bfs.js
@@ -0,0 +1,40 @@
+// Mock Graph Data
+const map = new Map();
+map.set("1", ["2", "3", "4"]);
+map.set("2", ["1", "5"]);
+map.set("3", ["1", "5"]);
+map.set("4", ["1", "6"]);
+map.set("5", ["2", "3", "7"]);
+map.set("6", ["4", "7"]);
+map.set("7", ["5", "6"]);
+
+export function fetchNeighbours(start) {
+  return map.get(start);
+}
+
+export function searchGraph(start) {
+  // 使用 queue 處理待走訪的節點
+  // 先將起點收進去
+  const queue = [start];
+
+  // 使用 Set 記錄參訪順序，不重復
+  const visited = new Set();
+
+  while (queue.length > 0) {
+    // 取得要參訪的節點
+    const node = queue.shift();
+    // 存入參訪路徑
+    visited.add(node);
+
+    // 取回鄰居節點
+    const neighbours = fetchNeighbours(node);
+
+    for (let neighbour of neighbours) {
+      // 若鄰居節點還沒拜訪過，放到隊列待後續處理
+      if (!visited.has(neighbour)) {
+        queue.push(neighbour);
+      }
+    }
+  }
+  return [...visited];
+}   
